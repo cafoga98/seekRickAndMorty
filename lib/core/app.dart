0b65e91122042c5951822cab5ae60b63121eb82d
cafoga98 <camiloforerogalindo@gmail.com>
2025-01-03 13:38:00 -0500

    Add MyApp to register all MaterialApp

---
+++ lib/core/app.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:get_it/get_it.dart';
+import 'package:seek_rickandmorty/core/config/auto_route/router.dart';
+import 'package:seek_rickandmorty/core/design/themes/main_theme.dart';
+
+class MyApp extends StatefulWidget {
+  final GetIt getIt;
+
+  const MyApp({
+    super.key,
+    required this.getIt,
+  });
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: customLightTheme(context),
+      darkTheme: customDarkTheme(context),
+      routeInformationParser: widget.getIt<AppRouter>().defaultRouteParser(),
+      routerDelegate: widget.getIt<AppRouter>().delegate(),
+      builder: (context, child) {
+        final mediaQuery = MediaQuery.of(context);
+        return Builder(
+          builder: (context) => MediaQuery(
+            data: mediaQuery.copyWith(
+              textScaler: TextScaler.noScaling,
+            ),
+            child: child!,
+          ),
+        );
+      },
+    );
+  }
+}

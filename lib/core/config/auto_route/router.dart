7a539ac8688bbd936b35ad1b00217cfed27ccdeb
cafoga98 <camiloforerogalindo@gmail.com>
2025-01-03 13:34:05 -0500

    Add auto_route to handle the router stack

---
+++ lib/core/config/auto_route/router.dart
@@ -0,0 +1,30 @@
+import 'package:auto_route/auto_route.dart';
+
+import 'pages.dart';
+
+part 'router.gr.dart';
+
+/// AppRouter
+///
+/// Manages the routing configuration for the Flutter application using the
+/// `auto_route` package. It defines the routes and their initial configuration.
+///
+/// Example usage:
+///
+/// ```dart
+/// final appRouter = AppRouter();
+/// ```
+@AutoRouterConfig(
+  replaceInRouteName: 'Page,Route',
+)
+class AppRouter extends RootStackRouter {
+
+  @override
+  RouteType get defaultRouteType => const RouteType.material();
+
+  @override
+  final List<AutoRoute> routes = [
+    AutoRoute(page: TemporaryRoute.page, path: '/', initial: true),
+    //AutoRoute(page: LoginRoute.page, path: '/'),
+  ];
+}

ee4f29abaf7a43487df9e733b2bc184445bc239c
cafoga98 <camiloforerogalindo@gmail.com>
2025-01-03 14:35:28 -0500

    Add failure text in gpl_api_config.dart and set up localizationsDelegates

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:get_it/get_it.dart';
 import 'package:seek_rickandmorty/core/config/auto_route/router.dart';
 import 'package:seek_rickandmorty/core/design/themes/main_theme.dart';
+import 'package:seek_rickandmorty/generated/l10n.dart';
 
 class MyApp extends StatefulWidget {
   final GetIt getIt;
@@ -25,6 +27,13 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
       darkTheme: customDarkTheme(context),
       routeInformationParser: widget.getIt<AppRouter>().defaultRouteParser(),
       routerDelegate: widget.getIt<AppRouter>().delegate(),
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       builder: (context, child) {
         final mediaQuery = MediaQuery.of(context);
         return Builder(

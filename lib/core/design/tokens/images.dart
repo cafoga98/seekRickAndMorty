e8ceac994708494021d34756877b21d4f18e837b
cafoga98 <camiloforerogalindo@gmail.com>
2025-01-05 15:53:55 -0500

    Add the new title image to episode page

--- lib/core/design/tokens/images.dart
+++ lib/core/design/tokens/images.dart
@@ -1,4 +1,5 @@
 class ImageRepository {
   ImageRepository._();
   static const characterTitle = 'assets/img/characters_title.png';
+  static const episodeTitle = 'assets/img/episodes_title.png';
 }
